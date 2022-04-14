162a7ea4e8498f444c9b2738b7e0659802c7dd82
Alysia Sherer <alysiasherer@utexas.edu>
2022-04-13 23:30:02 -0500

    table testing

---
+++ frontend/src/pages/columns.js
@@ -0,0 +1,18 @@
+//fill in accessor with name of json identifier
+//linkage to backend
+//edit to better names etc etc
+
+export const Columns = [
+    {
+        Header: 'Name',
+        accessor: ''
+    },
+    {
+        Header: 'Data',
+        accessor: ''
+    },
+    {
+        Header: 'Other Data',
+        accessor: ''
+    }
+]
