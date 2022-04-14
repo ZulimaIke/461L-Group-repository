c0cd705366436a9f0237f895866377eef4aa1cf0
Alex Tan <Alexander.goh.Tan67@gmail.com>
2022-04-13 20:13:27 -0500

    Updated home page
    -Links to create new account / sign into page
    -will remove buttons to those pages next

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 
 //Home page, initial start page w/ message
 const Home = () => {
 	
 return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Left',
-		alignItems: 'Left',
-		height: '100vh'
-	}}
-	>
+	<div>
 	<h1>
 		<text>
 			Welcome!{"\n"}
 		</text>
 	</h1>
 	
+	<li>
+		<Link to = "/createAcc">Create new account</Link>
+	</li>
+
+	<li>
+		<Link to = "/login">Sign into existing account</Link>
+	</li>
 	</div>
 );
 };
