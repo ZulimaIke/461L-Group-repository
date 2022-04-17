e8672965b6cbc08ef7bef0dc5c4b617f879a23d9
Alex Tan <Alexander.goh.Tan67@gmail.com>
2022-04-17 18:16:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import ssl
 import HWSet
 # comment out on deployment
 from flask_cors import CORS
+from User import User
 
 # uses 'frontend' because that is where our react app is stored
 app = Flask(__name__, static_folder="frontend/build", static_url_path="/")
@@ -41,16 +42,10 @@ def createAcc():
 
 @app.route('/hwSet/<setData>', methods=['GET', 'POST'])
 def createHWSet(setData: str):
-<<<<<<< HEAD
     #return {'response':'Hi'}
     # Client = MongoClient("mongodb+srv://2team:T32bfrH0L678xseI@finalproject.njqba.mongodb.net/FinalProject?retryWrites=true&w=majority")
     # db = Client.FinalProject
     hwSets = db.HWSets
-=======
-    Client = MongoClient("mongodb+srv://2team:T32bfrH0L678xseI@finalproject.njqba.mongodb.net/FinalProject?retryWrites=true&w=majority")
-    db = Client.FinalProject
-    HWSets = db.HWSets
->>>>>>> 538ae8aa2a7363b271489508edd8ede5de5f4b27
     data = setData.split("_")
     entry = {
         "Name": data[0],
@@ -58,7 +53,6 @@ def createHWSet(setData: str):
         "Availability": data[1],
         "Checkouts": {}
     }
-<<<<<<< HEAD
     # hwSet0 = entry["Hardware"] + "_" + entry["Capacity"] + "_" + entry["Availability"]
     hwSets.insert_one(entry)
     return {'response':'success'}
@@ -77,55 +71,6 @@ def getHWSets():
         existingSets.append(page)
     return jsonify(existingSets)
   
-=======
-    Client.close()
-    HWSet0 = entry["Hardware"] + "_" + entry["Capacity"] + "_" + entry["Availability"]
-    HWSets.insert_one(entry)
-    return jsonify(hwsetdata = HWSet0)
-
-#hardwareset checkin or checkout
-@app.route("/checkInOrOut/<setInfo>", methods=["GET"])
-# return statement in form of <whether successfull>_<HWSet capacity>_<HWSet availability>
-# <setInfo> in form:
-# <setName (either HWSet1 or 2)>_<key: either checkin, checkout>_<ProjectID>_<qty>
-def checkInOrOut(setInfo: str):
-    separated = setInfo.split('_')
-    setName = separated[0]
-    key = separated[1]
-    projectID = separated[2]
-    qty = separated[3]
-    success = 0
-
-    Client = MongoClient("mongodb+srv://2team:T32bfrH0L678xseI@finalproject.njqba.mongodb.net/FinalProject?retryWrites=true&w=majority")
-    db = Client.FinalProject
-    HWSets = db.HWSets
-    search = HWSets.find_one({"Name": setName})
-
-    #create HWSet object
-    HWSet0 = HWSet.HWSet(int(search["Availability"]), int(search["Capacity"]), search["Ckeckouts"])
-
-    if key == "checkout":
-        success = HWSet0.check_out(projectID, qty)
-    if key == "checkin":
-        success = HWSet0.check_in(projectID, qty)
-
-    # if checkin or checkout successful    
-    if success == 0:
-        HWSets.update_one(
-            {"Name": setName},
-            {'$set' : {
-                "Capacity": HWSet0.get_capacity(),
-                "Availability": HWSet0.get_availability(),
-                "Checkouts": HWSet0.get_checkOutQty() }})
-        Client.close()
-        data = "true" + str(HWSet0.get_capacity()) + "_" + str(HWSet0.get_availability())
-        return jsonify(HWSetData=data)
-    else:
-        Client.close()
-        data = "false" + str(HWSet0.get_capacity()) + "_" + str(HWSet0.get_availability())
-        return jsonify(HWSetData=data)   
-
->>>>>>> 538ae8aa2a7363b271489508edd8ede5de5f4b27
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
