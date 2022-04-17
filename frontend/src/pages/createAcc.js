17bd80a3525c23d0896136f3694e27c27cf21000
Alex Tan <Alexander.goh.Tan67@gmail.com>
2022-04-16 21:48:00 -0500

    changing login/create account
    using axios to handle data management of username/password
    
    Reworked login + createAcc

--- frontend/src/pages/createAcc.js
+++ frontend/src/pages/createAcc.js
@@ -4,30 +4,58 @@ import Button from "react-bootstrap/Button";
 import "./createAcc.css";
 import { Link } from "react-router-dom";
 
+const axios = require("axios").default;
+
+
 export default function CreateAcc() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [createdAcc, setCreatedAcc] = useState("");
+  const [createFail, setCreateFail] = useState(false);
+  const[failMessage, setFailMessage] = useState("");
 
-  function validateForm() {
-    return username.length > 0 && password.length > 0;
-  }
+  const handleUser = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePass = (e) => {
+    setPassword(e.target.value);
+  };
 
   function handleSubmit(event) {
-    event.preventDefault();
-  }
+    createFail(false);
+    if(!username){
+      createFail(true);
+      setFailMessage("Please enter a valid username");
+      return;
+    } 
+
+    axios.post("/user/createAcc", {
+      data:{
+        username: username,
+        password: password,
+      },
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    
+  };
 
   return (
     <div align="center" className="Login">
     <h1>Create a new account : </h1>
-      <Form onSubmit={handleSubmit}>
+      <Form>
         <Form.Group size="lg" controlId="username">
           <Form.Label>Username</Form.Label>
           <Form.Control
             autoFocus
             type="username"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={handleUser}
           />
         </Form.Group>
         <Form.Group size="lg" controlId="password">
@@ -35,29 +63,19 @@ export default function CreateAcc() {
           <Form.Control
             type="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={handlePass}
           />
         </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}
-         onClick={() => {
-                    fetch("http://127.0.0.1:5000/create_acc/" + username + "_" + password)
-                        .then(response => 
-                            response.json()
-                        )
-                        .then(data => {
-                            setCreatedAcc(data.createdAcc)
-			    console.log(createdAcc)
-                        })
-                        .catch(error => {
-                            console.log(error)
-                        })
-                        }}
-        >
+        <Button block size="lg" type="submit" onClick={handleSubmit}>
           Create Account
         </Button>
         <li>
           <Link to = "/">Back</Link>
         </li>
+        <li>
+          <Link to = "/login">Login to existing account</Link>
+        </li>
+        <h3>{failMessage}</h3>
       </Form>
     </div>
   );
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -2,32 +2,74 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./login.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+const axios = require("axios").default;
 
 export default function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginSuccess, setLoginSuccess] = useState("");
+  const navigateTo = useNavigate();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [loginFail, setLoginFail] = useState(false);
+  const[failMessage, setFailMessage] = useState();
 
-  function validateForm() {
-    return username.length > 0 && password.length > 0;
-  }
+  const handleUser = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePass = (e) => {
+    setPassword(e.target.value);
+  };
 
   function handleSubmit(event) {
-    event.preventDefault();
-  }
+    //no login fail
+    setLoginFail(false);
+    //Check for fail if missing username/password
+    if(!username){
+      setLoginFail(true);
+      setFailMessage("Please input username");
+      return;
+    } else if(!password){
+      setLoginFail(true);
+      setFailMessage("Please input password");
+      return;
+    }
+    
+    axios.post('/user/login', {
+      data: {
+        username: username,
+        password: password,
+      },
+    })
+    .then(function (response) {
+      let requestResponse = response.data;
+      if(requestResponse === "invalid username"){
+        setLoginFail(true);
+        setFailMessage("Invalid username");
+      } else if(requestResponse === "invalid password"){
+        setLoginFail(true);
+        setFailMessage("Invalid password");
+      } else {
+        navigateTo('/postLogin');
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  };
 
   return (
     <div align="center" className="Login">
     <h1>Sign into an existing account</h1>
-      <Form onSubmit={handleSubmit}>
+      <Form>
         <Form.Group size="lg" controlId="username">
           <Form.Label>Username</Form.Label>
           <Form.Control
             autoFocus
             type="username"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={handleUser}
           />
         </Form.Group>
         <Form.Group size="lg" controlId="password">
@@ -35,31 +77,24 @@ export default function Login() {
           <Form.Control
             type="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={handlePass}
           />
         </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}
-         onClick={() => {
-                    fetch("http://127.0.0.1:5000/login/" + username + "_" + password)
-                        .then(response => 
-                            response.json()
-                        )
-                        .then(data => {
-                            setLoginSuccess(data.loginSuccess)
-			    console.log(loginSuccess)
-                        })
-                        .catch(error => {
-                            console.log(error)
-                        })
-                        }}
-        >
+        <Button block size="lg" type="submit" onClick={handleSubmit}>
           Login
         </Button>
 
         <li>
           <Link to = "/">Back</Link>
         </li>
+        <li>
+          <Link to = "/createAcc">Create new account</Link>
+        </li>
+        <div>
+          <h3>{failMessage}</h3>
+        </div>
       </Form>
+     
     </div>
   );
 }
