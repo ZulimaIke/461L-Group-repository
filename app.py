bd07ed4665263d6b4d3607af77bde1ce3af001dd
Alex Tan <Alexander.goh.Tan67@gmail.com>
2022-04-17 01:24:07 -0500

    create account backend
    Link to mongodb and testing with inserting test accounts

--- app.py
+++ app.py
@@ -13,6 +13,31 @@ app = Flask(__name__, static_folder="frontend/build", static_url_path="")
 
 # comment out on deployment
 CORS(app)
+mongoPass = "T32bfrH0L678xseI"
+c = MongoClient(f"mongodb+srv://2team:{mongoPass}@finalproject.njqba.mongodb.net/FinalProject?retryWrites=true&w=majority") 
+db = c.FinalProject
+
+users_db = db['Users']
+
+@app.route('/user/createAcc', methods = ["POST"])
+def createAcc():
+    requestData = json.loads(request.data)
+    payload = requestData['data']
+
+    username = payload['username']
+    password = payload['password']
+
+    user = Users(username, password)
+
+    collection = c.FinalProject.Users
+
+    try:
+        user_id = c.FinalProject.insert_one(user.toDatabase()).inserted_id
+    except Exception as e:
+        print(e, flush = True)
+        return;
+    else:
+        return;
 
 
     
