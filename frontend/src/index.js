3b7aa0be67dc3ddd8fdc4ce4f62c5a41139e89d5
Joseph Huynh <61037555+josephvhuynh@users.noreply.github.com>
2022-04-21 19:44:46 -0500

    try hashrouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-Import { BrowserRouter } from 'react-router-dom';
+Import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
